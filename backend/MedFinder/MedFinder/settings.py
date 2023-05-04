5facbab65a91ed98012f8913050b7e11cd31873c
ParsaFouladi <parsa_fouladi@yahoo.com>
2023-05-05 02:53:21 +0300

    update: doctor_app is added to admin panel and first migration is made

--- backend/MedFinder/MedFinder/settings.py
+++ backend/MedFinder/MedFinder/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'doctor_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,18 @@ WSGI_APPLICATION = 'MedFinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'dbMedFinder',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'tintin24',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
+
     }
 }
 
