cb23807d2978b9f792658630c0c3f2fb59342929
ParsaFouladi <parsa_fouladi@yahoo.com>
2023-05-05 03:23:06 +0300

    feature: basic get doctor information is added to the project

--- backend/MedFinder/doctor_app/views.py
+++ backend/MedFinder/doctor_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Doctor
+from .serializers import DoctorSerializer
 
 # Create your views here.
+class DoctorDetailApiView(generics.RetrieveAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
