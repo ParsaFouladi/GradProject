2e9b2b45829882e5bffeee466370e8be65c7a28d
ParsaFouladi <parsa_fouladi@yahoo.com>
2023-05-08 00:17:35 +0300

    feature: create user while creating the doctor

--- backend/MedFinder/doctor_app/serializers.py
+++ backend/MedFinder/doctor_app/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('username','password',)
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
@@ -17,3 +17,11 @@ class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = '__all__'
+    
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        department_data = validated_data.pop('department')
+        department = Department.objects.get_or_create(**department_data)[0]
+        user = User.objects.create_user(**user_data)
+        doctor = Doctor.objects.create(user=user,department=department,**validated_data)
+        return doctor
--- backend/MedFinder/doctor_app/views.py
+++ backend/MedFinder/doctor_app/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import Doctor
-from .serializers import DoctorSerializer
+from .serializers import DoctorSerializer,UserSerializer
 
 # Create your views here.
 class DoctorDetailApiView(generics.RetrieveAPIView):
@@ -10,3 +10,13 @@ class DoctorDetailApiView(generics.RetrieveAPIView):
 class DoctorCreateApiView(generics.CreateAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+
+    # def perform_create(self, serializer):
+    #     # Create a new user
+    #     user_data = serializer.validated_data.get('user')
+    #     user_serializer = UserSerializer(data=user_data)
+    #     user_serializer.is_valid(raise_exception=True)
+    #     user = user_serializer.save()
+
+    #     # Set the created user as the user for the doctor
+    #     serializer.save(user=user)
