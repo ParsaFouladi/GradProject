9ca852713f955a1c59ed06dca5cecba65e1938fa
ParsaFouladi <parsa_fouladi@yahoo.com>
2023-05-20 15:10:36 +0300

    feature: basic permissions are added for patients too

--- backend/MedFinder/patient_app/views.py
+++ backend/MedFinder/patient_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Patient
 from .serializers import PatientSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwner
 
 class PatientDetailApiView(generics.RetrieveAPIView):
     queryset = Patient.objects.all()
@@ -16,6 +19,7 @@ class PatientListApiView(generics.ListAPIView):
     serializer_class = PatientSerializer
 
 class PatientUpdateApiView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated, IsOwner]
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
 
