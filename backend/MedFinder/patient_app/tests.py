a14bdb69fe6e0ec7fbb9011aab9a1f83ad783e64
Roa'a Alqaisi <72887182+RoaaAlqaisi@users.noreply.github.com>
2023-05-23 15:02:05 +0300

    patient_app-tests updated

--- backend/MedFinder/patient_app/tests.py
+++ backend/MedFinder/patient_app/tests.py
@@ -4,8 +4,10 @@ from .models import Patient, PatientContactInfo, Insurance
 from .serializers import PatientSerializer
 from rest_framework.test import APIClient
 from rest_framework import status
+from django.urls import reverse
 
-class PatientModelTest(TestCase):
+
+class PatientModelTest(TestCase): 
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
         self.patient = Patient.objects.create(user=self.user, first_name='John', last_name='Doe')
@@ -14,6 +16,11 @@ class PatientModelTest(TestCase):
         self.assertEqual(self.patient.user, self.user)
         self.assertEqual(self.patient.first_name, 'John')
         self.assertEqual(self.patient.last_name, 'Doe')
+
+    def test_patient_str_representation(self):
+        patient = Patient.objects.create(user=self.user, first_name='John', last_name='Doe')
+        self.assertEqual(str(patient), 'John Doe')
+
 
 class PatientSerializerTest(TestCase):
     def setUp(self):
@@ -106,3 +113,134 @@ class PatientAPITest(TestCase):
         self.assertEqual(str(insurance.valid_from), '2023-01-01')
         self.assertEqual(str(insurance.valid_to), '2024-01-01')
 
+
+    def test_get_patient_list(self):
+        url = reverse('patient-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)  # Assuming there is only one patient in the database
+
+    def test_update_patient(self):
+        url = reverse('patient-detail', kwargs={'pk': self.patient.pk})
+        data = {
+            'first_name': 'Updated First Name',
+            'last_name': 'Updated Last Name',
+            'contact_info': {
+                'phone_number': '987654321',
+                'address': 'Updated Address'
+            }
+        }
+
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.patient.refresh_from_db()
+        self.assertEqual(self.patient.first_name, 'Updated First Name')
+        self.assertEqual(self.patient.last_name, 'Updated Last Name')
+        self.assertEqual(self.patient.contact_info.phone_number, '987654321')
+        self.assertEqual(self.patient.contact_info.address, 'Updated Address')
+
+    def test_delete_patient(self):
+        url = reverse('patient-detail', kwargs={'pk': self.patient.pk})
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Patient.objects.filter(pk=self.patient.pk).exists())
+
+
+    def test_create_patient_missing_fields(self):
+        url = reverse('patient-create')
+        data = {
+            # Missing 'user' field
+            # Missing 'first_name' field
+            # Missing 'last_name' field
+            'contact_info': {
+                'phone_number': '987654321',
+                'address': '456 Test Street'
+            },
+            'insurance': {
+                'insurance_number': '987654321',
+                'provider': 'Test Provider',
+                'valid_from': '2023-01-01',
+                'valid_to': '2024-01-01'
+            }
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {
+            'user': ['This field is required.'],
+            'first_name': ['This field is required.'],
+            'last_name': ['This field is required.'],
+        })
+        self.assertEqual(Patient.objects.count(), 1)  # No new patient should be created
+
+
+    def test_create_patient_invalid_data_type(self):
+        url = reverse('patient-create')
+        data = {
+            'user': 'invalid_id',  # Invalid user ID data type
+            'first_name': 'Jane',
+            'last_name': 'Smith',
+            'contact_info': {
+                'phone_number': '987654321',
+                'address': '456 Test Street'
+            },
+            'insurance': {
+                'insurance_number': '987654321',
+                'provider': 'Test Provider',
+                'valid_from': '2023-01-01',
+                'valid_to': '2024-01-01'
+            }
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {
+            'user': ['Invalid pk "invalid_id" - object does not exist.'],
+        })
+
+    def test_create_patient_with_existing_user(self):
+        url = reverse('patient-create')
+        data = {
+            'user': self.user.id,  # Existing user ID
+            'first_name': 'Jane',
+            'last_name': 'Smith',
+            'contact_info': {
+                'phone_number': '987654321',
+                'address': '456 Test Street'
+            },
+            'insurance': {
+                'insurance_number': '987654321',
+                'provider': 'Test Provider',
+                'valid_from': '2023-01-01',
+                'valid_to': '2024-01-01'
+            }
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {
+            'user': ['This user already has a patient entry.'],
+        })
+
+
+
+
+class PatientContactInfoModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.patient = Patient.objects.create(user=self.user, first_name='John', last_name='Doe')
+        self.contact_info = PatientContactInfo.objects.create(patient=self.patient, phone_number='123456789', address='123 Test Street')
+
+    def test_contact_info_str_representation(self):
+        self.assertEqual(str(self.contact_info), 'Contact info for John Doe')
+
+class InsuranceModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.patient = Patient.objects.create(user=self.user, first_name='John', last_name='Doe')
+        self.insurance = Insurance.objects.create(patient=self.patient, insurance_number='123456789', provider='Test Provider', valid_from='2023-01-01', valid_to='2024-01-01')
+
+    def test_insurance_str_representation(self):
+        self.assertEqual(str(self.insurance), 'Insurance: 123456789')
