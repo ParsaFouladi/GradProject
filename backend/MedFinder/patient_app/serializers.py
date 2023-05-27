ba040e83c169d5ee0ec6f8852565ed842e1a3d69
ParsaFouladi <parsa_fouladi@yahoo.com>
2023-05-28 01:20:14 +0300

    update: insurance problem in patient serializer is fixed

--- backend/MedFinder/patient_app/serializers.py
+++ backend/MedFinder/patient_app/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Patient, Insurance
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from typing import Dict, List
+from rest_framework.exceptions import ValidationError
 
-class UserSerializerWrite(serializers.ModelSerializer):
+
+class UserSerializerWritePatient(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username','password',)
@@ -17,10 +21,17 @@ class InsuranceSerializer(serializers.ModelSerializer):
         model = Insurance
         fields = ('id','name', 'number', 'provider', 'valid_from', 'valid_to' )
 
+class InsuranceSerializerInPatient(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = Insurance
+        fields = ('id',)
+
 class PatientSerializer(serializers.ModelSerializer):
     
-    insurance = InsuranceSerializer()
-    user = UserSerializerWrite()
+    insurance = InsuranceSerializerInPatient(many=True, required=False)
+    user = UserSerializerWritePatient()
+    
 
     class Meta:
         model = Patient
@@ -28,18 +39,20 @@ class PatientSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         
-        insurance_data = validated_data.pop('insurance')
-
-        
-
+        insurance_data = validated_data.pop('insurance', [])
         
         user_data = validated_data.pop('user')
         user = User.objects.create_user(**user_data)
 
-        patient = Patient.objects.create(user=user, **validated_data)
+        patient = Patient.objects.create(user=user,**validated_data)
 
-        
-        insurance = Insurance.objects.create(patient=patient,**insurance_data)
+        for insurance_id in insurance_data:
+            try:
+                insurance = Insurance.objects.get(pk=insurance_id['id'])
+                patient.insurance.add(insurance)
+            except Insurance.DoesNotExist:
+                raise ValidationError(f"Insurance with ID {insurance_id} does not exist.")
+
         return patient
 
     def update(self, instance, validated_data):
