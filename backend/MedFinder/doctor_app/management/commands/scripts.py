a99e83ff38343b17b1717874d345f71c3cf81035
ParsaFouladi <parsa_fouladi@yahoo.com>
2023-05-28 23:44:13 +0300

    add: scripts for transferring excel to database is added

---
+++ backend/MedFinder/doctor_app/management/commands/scripts.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from ...models import ScrapedDoctors
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Imports scraped data into the ScrapedDoctors model'
+
+    def handle(self, *args, **options):
+        # Read the Excel file into a pandas DataFrame
+        data = pd.read_excel(r'D:\GradProject\GradProject\scraping\data.xlsx')
+
+        # Iterate over the rows of the DataFrame and create ScrapedDoctors instances
+        for _, row in data.iterrows():
+            name = row['name']
+            location = row['location']
+            specialty = row['specialty']
+            experience = row['experience']
+            description = row['description']
+            url = row['url']
+            image_url = row['image_url']
+
+            # Create a new instance of ScrapedDoctors
+            scraped_doctor = ScrapedDoctors(
+                name=name,
+                location=location,
+                specialty=specialty,
+                experience=experience,
+                description=description,
+                url=url,
+                image_url=image_url
+            )
+
+            # Save the instance to the database
+            scraped_doctor.save()
+
+        self.stdout.write(self.style.SUCCESS('Scraped data imported successfully.'))
