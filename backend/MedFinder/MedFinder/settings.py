ee066c626e8030d5f119e0ffeb9dc912b29ee6f8
ParsaFouladi <parsa_fouladi@yahoo.com>
2023-05-28 23:42:48 +0300

    update: page size is changed for pagination

--- backend/MedFinder/MedFinder/settings.py
+++ backend/MedFinder/MedFinder/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
     ),
     # Pagination
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 2,
+    "PAGE_SIZE": 10,
 }
 
 SPECTACULAR_SETTINGS = {
