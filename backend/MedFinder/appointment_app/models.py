3c95a7c87399a09376cd0eccfc05a46157f7f5f5
ParsaFouladi <parsa_fouladi@yahoo.com>
2023-06-21 22:44:58 +0300

    add: basic cruds for appointment app is added

--- backend/MedFinder/appointment_app/models.py
+++ backend/MedFinder/appointment_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from doctor_app.models import Doctor
+from patient_app.models import Patient
 
 # Create your models here.
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['date', 'time']
+
+    def __str__(self):
+        return str(self.doctor) + ' ' + str(self.patient) + ' ' + str(self.date) + ' ' + str(self.time)
+
+
+
+
+
+
