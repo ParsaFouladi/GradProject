921615e2503c39b66ab7b4dc74cde5283fefbbd5
Roa'a Alqaisi <72887182+RoaaAlqaisi@users.noreply.github.com>
2023-06-22 18:44:46 +0300

    updated views

--- backend/MedFinder/google_app/views.py
+++ backend/MedFinder/google_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
     client_class = OAuth2Client
-    callback_url = 'http://localhost:8000/'
+    callback_url = 'http://localhost:8000/accounts/google/login/callback/'  # URL to handle callback from Google
