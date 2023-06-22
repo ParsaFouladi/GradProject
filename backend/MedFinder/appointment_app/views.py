8891146763852415c2bf8da604db556606681aff
ParsaFouladi <parsa_fouladi@yahoo.com>
2023-06-22 14:05:13 +0300

    update: appointment based on the doctor and patient id is added

--- backend/MedFinder/appointment_app/views.py
+++ backend/MedFinder/appointment_app/views.py
@@ -10,6 +10,23 @@ class AppointmentListApiView(generics.ListAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
+    #Get the appointments of the current user
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     return Appointment.objects.filter(patient=user)
+
+    #Get appointments based on the patient id
+    def get_queryset(self):
+        queryset=super().get_queryset()
+        patient_id = self.request.query_params.get('patient_id',None)
+        doctor_id = self.request.query_params.get('doctor_id',None)
+        if patient_id:
+            queryset=queryset.filter(patient_id=patient_id)
+        if doctor_id:
+            queryset=queryset.filter(doctor_id=doctor_id)
+        
+        return queryset
+
 class AppointmentCreateApiView(generics.CreateAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
